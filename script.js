c11b0d49a9a9aa3caf1356780e49192dfd9f7441
wael ahmed <w.ahmed020@gmail.com>
2021-03-19 18:08:07 +0000

    css styling updates

--- script.js
+++ script.js
@@ -19,21 +19,33 @@ let mode = "light";
   });
 
 
+  var time = moment().format('MMMM Do YYYY, h:mm:ss a');
+  $(".time").text(time)
+
+
 
   //Time
   var timeDisplayEl = document.getElementById('time-display');
 
   // handle displaying the time
-  function displayTime() {
-    var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
-    timeDisplayEl.text(rightNow);
-  }
+//   function displayTime() {
+//     var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
+//     timeDisplayEl.text(rightNow);
+//   }
 
 
+  //Jquery - submit button
   $(".saveBtn").on("click", function (){
       
-    var activity = $(this).siblings(".description".val())
+    var activity = $(this).siblings(".description").val()
+    var time = $(this).parent().attr("id")
 
-    console.log(activity)
+    console.log(activity);
+    console.log(time);
 
-  })
+    localStorage.setItem(time, activity)
+
+  });
+
+  $("#9 .description").val(localStorage.getItem("9"));
+  $("#10 .description").val(localStorage.getItem("10"));
