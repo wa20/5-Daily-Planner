b2a1bfa4e51247fd36fa6929211ee641968e04b9
wael ahmed <w.ahmed020@gmail.com>
2021-03-19 19:44:55 +0000

    background immage add

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ let mode = "light";
   });
 
 
-  var time = moment().format('MMMM Do YYYY, h:mm:ss a');
+  var time = moment().format('DD MM YY | HH:MM A');
   $(".time").text(time)
 
 
@@ -27,11 +27,6 @@ let mode = "light";
   //Time
   var timeDisplayEl = document.getElementById('time-display');
 
-  // handle displaying the time
-//   function displayTime() {
-//     var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
-//     timeDisplayEl.text(rightNow);
-//   }
 
 
   //Jquery - submit button
@@ -49,3 +44,10 @@ let mode = "light";
 
   $("#9 .description").val(localStorage.getItem("9"));
   $("#10 .description").val(localStorage.getItem("10"));
+  $("#11 .description").val(localStorage.getItem("11"));
+  $("#12 .description").val(localStorage.getItem("12"));
+  $("#13 .description").val(localStorage.getItem("13"));
+  $("#14 .description").val(localStorage.getItem("14"));
+  $("#15 .description").val(localStorage.getItem("15"));
+  $("#16 .description").val(localStorage.getItem("16"));
+  $("#17 .description").val(localStorage.getItem("17"));
