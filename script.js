7d30f05165ebc6ebdf736050a482f6a9702f4aa6
wael ahmed <w.ahmed020@gmail.com>
2021-03-20 22:32:40 +0000

    time block colors updates

--- script.js
+++ script.js
@@ -44,21 +44,6 @@ let mode = "light";
   });
 
 
-
-$(".clearBtn").on("click", function () {
-
-    var activity = $(this).siblings(".description").val()
-    var time = $(this).parent().attr("id")
-
-    localStorage.clear(time, activity);       // clear local storage
-    location.reload();          // reload page
-});
-
-
-//Jquery - clear button
-$("")
-
-
   $("#8 .description").val(localStorage.getItem("8"));
   $("#9 .description").val(localStorage.getItem("9"));
   $("#10 .description").val(localStorage.getItem("10"));
@@ -70,3 +55,69 @@ $("")
   $("#16 .description").val(localStorage.getItem("16"));
   $("#17 .description").val(localStorage.getItem("17"));
   $("#18 .description").val(localStorage.getItem("18"));
+
+
+
+
+// $(".clearBtn").on("click", function (){
+
+//     var activities = $(this).siblings(".description").val()
+//     var timer = $(this).parent().attr("id")
+
+//     localStorage.clear(timer, activities);  
+//     location.reload();                
+// });
+
+
+//Jquery - clear button
+// $("")
+
+// $("#8 .description").val(localStorage.clear("8"));
+
+
+
+
+
+
+//current time tracker
+function currentTime () {
+ 
+    var  currentHour = moment().hour(); 
+    
+    $(" ").each(function() {
+     
+    var timeBlock = $(this).parent().attr("id");
+
+    if(timeBlock === currentHour) {
+        $(this).addClass("presentTime");
+        $(this).removeClass("pastTime");
+        $(this).removeClass("futureTime");
+
+
+    } else 
+        if (timeBlock > currentHour) {
+            $(this).removeClass("presentTime");
+            $(this).addClass("pastTime");
+            $(this).removeClass("futureTime");
+        
+    } else 
+        if (timeBlock < currentHour) {
+            $(this).removeClass("presentTime");
+            $(this).removeClass("pastTime");
+            $(this).addClass("futureTime");
+        }
+
+    })
+
+currentHour()
+
+}
+
+
+// if (now > 9) {
+//     $("#time-0").addClass("past");
+// } else if (now >= 9 && now < 10) {
+//     $("#time-0").addClass("present");
+// } else if (now < 9) {
+//     $("#time-0").addClass("future");
+// }
