cb0c7217be6caaba7ae56f578ea3e62f0d15742f
wael ahmed <w.ahmed020@gmail.com>
2021-03-21 22:05:09 +0000

    readme updates and font colour fixes|

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # 5-Daily-Planner
+
+A web and mobile responsive daily work plannig application that allows users to save events for each working hour of the day. 
+
+ability to add and save events to each time block by pressing the save button.
+
+a clear all button will clear all saved data in local storage and reset the page.
+
+
+<img src="./Assets/dark-mode.png" width="250"/>
+<img src="./Assets/light-mode.png" width="250"/>
+
+
--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 //Theme switcher
 const bodyEl = document.querySelector("body");
 const headerEl = document.getElementById("sidebar");
@@ -6,186 +5,166 @@ const themeSwitcher = document.querySelector("#theme-switcher");
 
 let mode = "light";
 
-  themeSwitcher.addEventListener("click", function () {
-    if (mode === "light") {
-      mode = "dark";
-      bodyEl.setAttribute("class", "dark");
-      headerEl.setAttribute("class", "sidebarDark");
-    } else {
-      mode = "light";
-      bodyEl.setAttribute("class", "light");
-      headerEl.setAttribute("class", "sidebar");
-   
-    }
-  });
-
-// time and date elements
-  var date = moment().format('Do MMMM YYYY');
-  $(".date").text(date)
-
-  var day = moment().format('dddd');
-  $(".day").text(day)
-
-  var time = moment().format('HH:MM A');
-  $(".time").text(time)
-
-
-
-
-
-  //Time displ
-//   var timeDisplayEl = document.getElementById('time-display');
-
-
-
-  //Jquery - submit button
-  $(".saveBtn").on("click", function (){
-    var activity = $(this).siblings(".description").val()
-    var time = $(this).parent().attr("id")
-
-    console.log(activity);
-    console.log(time);
-
-    localStorage.setItem(time, activity)
-  });
-
-  $("")
-  $("#8 .description").val(localStorage.getItem("8"));
-  $("#9 .description").val(localStorage.getItem("9"));
-  $("#10 .description").val(localStorage.getItem("10"));
-  $("#11 .description").val(localStorage.getItem("11"));
-  $("#12 .description").val(localStorage.getItem("12"));
-  $("#13 .description").val(localStorage.getItem("13"));
-  $("#14 .description").val(localStorage.getItem("14"));
-  $("#15 .description").val(localStorage.getItem("15"));
-  $("#16 .description").val(localStorage.getItem("16"));
-  $("#17 .description").val(localStorage.getItem("17"));
-  $("#18 .description").val(localStorage.getItem("18"));
-
-
-
-$(".clear").text("CLEAR ALL")
-
-$(".clear").on("click", function (){
-
-    var clear = window.confirm("WARNING: THIS WILL CLEAR ALL DATA?")
-
-    if(!clear) {
-        return
-    } else {
-
-    var activities = $(this).siblings(".description").val()
-    var timer = $(this).parent().attr("id")
-
-    localStorage.clear(timer, activities);  
-    location.reload();   
-    
-    }
+themeSwitcher.addEventListener("click", function () {
+  if (mode === "light") {
+    mode = "dark";
+    bodyEl.setAttribute("class", "dark");
+    headerEl.setAttribute("class", "sidebarDark");
+  } else {
+    mode = "light";
+    bodyEl.setAttribute("class", "light");
+    headerEl.setAttribute("class", "sidebar");
+  }
 });
 
+// time and date elements
+var date = moment().format("Do MMMM YYYY");
+$(".date").text(date);
 
-var  currentHour = moment().hour(); 
+var day = moment().format("dddd");
+$(".day").text(day);
 
+var time = moment().format("HH:MM A");
+$(".time").text(time);
+
+//Jquery - submit button
+$(".saveBtn").on("click", function () {
+  var activity = $(this).siblings(".description").val();
+  var time = $(this).parent().attr("id");
+
+  console.log(activity);
+  console.log(time);
+
+  localStorage.setItem(time, activity);
+});
+
+$("");
+$("#8 .description").val(localStorage.getItem("8"));
+$("#9 .description").val(localStorage.getItem("9"));
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#13 .description").val(localStorage.getItem("13"));
+$("#14 .description").val(localStorage.getItem("14"));
+$("#15 .description").val(localStorage.getItem("15"));
+$("#16 .description").val(localStorage.getItem("16"));
+$("#17 .description").val(localStorage.getItem("17"));
+$("#18 .description").val(localStorage.getItem("18"));
+
+//clear button
+
+$(".clear").text("CLEAR ALL");
+
+$(".clear").on("click", function () {
+  var clear = window.confirm("WARNING: THIS WILL CLEAR ALL DATA?");
+
+  if (!clear) {
+    return;
+  } else {
+    var activities = $(this).siblings(".description").val();
+    var timer = $(this).parent().attr("id");
+
+    localStorage.clear(timer, activities);
+    location.reload();
+  }
+});
+
+// timeblock colour settings.
+
+var currentHour = moment().hour();
 
 if (currentHour > 8) {
-    $(".time8").addClass("pastTime");
+  $(".time8").addClass("pastTime");
 } else if (currentHour == 8) {
-    $(".time8").addClass("presentTime");
+  $(".time8").addClass("presentTime");
 } else if (currentHour < 8) {
-    $(".time8").addClass("futureTime");
+  $(".time8").addClass("futureTime");
 }
 
 if (currentHour > 9) {
-    $(".time9").addClass("pastTime");
+  $(".time9").addClass("pastTime");
 } else if (currentHour == 9) {
-    $(".time9").addClass("presentTime");
+  $(".time9").addClass("presentTime");
 } else if (currentHour < 9) {
-    $(".time9").addClass("futureTime");
+  $(".time9").addClass("futureTime");
 }
 
 if (currentHour > 10) {
-    $(".time10").addClass("pastTime");
+  $(".time10").addClass("pastTime");
 } else if (currentHour == 10) {
-    $(".time10").addClass("presentTime");
+  $(".time10").addClass("presentTime");
 } else if (currentHour < 10) {
-    $(".time10").addClass("futureTime");
+  $(".time10").addClass("futureTime");
 }
 
 if (currentHour > 11) {
-    $(".time11").addClass("pastTime");
+  $(".time11").addClass("pastTime");
 } else if (currentHour == 11) {
-    $(".time11").addClass("presentTime");
+  $(".time11").addClass("presentTime");
 } else if (currentHour < 11) {
-    $(".time11").addClass("futureTime");
+  $(".time11").addClass("futureTime");
 }
 
 if (currentHour > 12) {
-    $(".time12").addClass("pastTime");
+  $(".time12").addClass("pastTime");
 } else if (currentHour == 12) {
-    $(".time12").addClass("presentTime");
+  $(".time12").addClass("presentTime");
 } else if (currentHour < 12) {
-    $(".time12").addClass("futureTime");
+  $(".time12").addClass("futureTime");
 }
 
 if (currentHour > 13) {
-    $(".time13").addClass("pastTime");
+  $(".time13").addClass("pastTime");
 } else if (currentHour == 13) {
-    $(".time13").addClass("presentTime");
+  $(".time13").addClass("presentTime");
 } else if (currentHour < 13) {
-    $(".time13").addClass("futureTime");
+  $(".time13").addClass("futureTime");
 }
 
 if (currentHour > 14) {
-    $(".time14").addClass("pastTime");
+  $(".time14").addClass("pastTime");
 } else if (currentHour == 14) {
-    $(".time14").addClass("presentTime");
+  $(".time14").addClass("presentTime");
 } else if (currentHour < 14) {
-    $(".time14").addClass("futureTime");
+  $(".time14").addClass("futureTime");
 }
 
 if (currentHour > 14) {
-    $(".time14").addClass("pastTime");
+  $(".time14").addClass("pastTime");
 } else if (currentHour == 14) {
-    $(".time14").addClass("presentTime");
+  $(".time14").addClass("presentTime");
 } else if (currentHour < 14) {
-    $(".time14").addClass("futureTime");
+  $(".time14").addClass("futureTime");
 }
 
 if (currentHour > 15) {
-    $(".time15").addClass("pastTime");
+  $(".time15").addClass("pastTime");
 } else if (currentHour == 15) {
-    $(".time15").addClass("presentTime");
+  $(".time15").addClass("presentTime");
 } else if (currentHour < 15) {
-    $(".time15").addClass("futureTime");
+  $(".time15").addClass("futureTime");
 }
 
 if (currentHour > 16) {
-    $(".time16").addClass("pastTime");
+  $(".time16").addClass("pastTime");
 } else if (currentHour == 16) {
-    $(".time16").addClass("presentTime");
+  $(".time16").addClass("presentTime");
 } else if (currentHour < 16) {
-    $(".time16").addClass("futureTime");
+  $(".time16").addClass("futureTime");
 }
 
 if (currentHour > 17) {
-    $(".time17").addClass("pastTime");
+  $(".time17").addClass("pastTime");
 } else if (currentHour == 17) {
-    $(".time17").addClass("presentTime");
+  $(".time17").addClass("presentTime");
 } else if (currentHour < 17) {
-    $(".time17").addClass("futureTime");
+  $(".time17").addClass("futureTime");
 }
 
 if (currentHour > 18) {
-    $(".time18").addClass("pastTime");
+  $(".time18").addClass("pastTime");
 } else if (currentHour == 18) {
-    $(".time18").addClass("presentTime");
+  $(".time18").addClass("presentTime");
 } else if (currentHour < 18) {
-    $(".time18").addClass("futureTime");
+  $(".time18").addClass("futureTime");
 }
-
-
-
-
-
-
-
